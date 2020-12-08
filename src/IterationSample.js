bbf47cb80a57ff4aadf4a9bbe2cba778c0c4a7e2
funhappyit <fun.happyit@gmail.com>
2020-12-08 20:06:45 +0900

    배열 추가 삭제

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -9,9 +9,34 @@ const IterationSample = () => {
   const [inputText, setInputText] = useState("");
   const [nextId, setNextId] = useState(5); //새로운 항목을 추가할 때 사용할 id
 
-  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId, // nextId 값을 id로 설정하고
+      text: inputText,
+    });
+    setNextId(nextId + 1); //nextId값에 1을 더해 준다.
+    setNames(nextNames); //names 값을 업데이트 한다.
+    setInputText(""); //inputText를 비운다.
+  };
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
 
-  return <ul>{nameList}</ul>;
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>;
+    </>
+  );
 };
 
 export default IterationSample;
